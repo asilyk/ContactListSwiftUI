d54ada36e50dfdcf819b87751b0fa84f956a0f84
fabet <asilyk112@gmail.com>
2022-05-03 22:33:12 +0300

    Setup ContentView

--- ContactListSwiftUI/Views/ContentView.swift
+++ ContactListSwiftUI/Views/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let persons = Person.getPersons()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            ContactsView(persons: persons)
+                .tabItem {
+                    Image(systemName: "person.3")
+                    Text("Contacts")
+                }
+
+            NumbersView(persons: persons)
+                .tabItem {
+                    Image(systemName: "phone.fill")
+                    Text("Numbers")
+                }
+        }
     }
 }
 
